6d9c2c199381c1842fe1e3caf132a0e590a52c47
Anna Llambias Maw <87076233+Anna-LM@users.noreply.github.com>
2025-10-31 19:46:30 -0600

    create list of desired towel designs

--- main.py
+++ main.py
@@ -2,11 +2,22 @@
 f = open("example.txt")
 
 #read the first line of the file
-towel_inputs = (f.readline())
+towel_inputs = (f.readline()).strip('\n')
 
 #format the line into a list of each towel option
 towel_inputs=towel_inputs.split(', ')
 print(towel_inputs)
+
+desired_towels=[]
+#make a list of all the final towel designs
+for line in f:
+    desired_towels.append((line.strip('\n')))
+
+#remove first element which was blank
+desired_towels=desired_towels[1:]
+
+print(desired_towels[1:])
+ 
 
 #close file
 f.close()
