9d1f596dbf61348139f7af95b59bd9cacff15684
Anna Llambias Maw <87076233+Anna-LM@users.noreply.github.com>
2025-10-31 19:38:42 -0600

    format towel input string to list

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+#Open file name example.txt
 f = open("example.txt")
+
+#read the first line of the file
 towel_inputs = (f.readline())
+
+#format the line into a list of each towel option
+towel_inputs=towel_inputs.split(', ')
 print(towel_inputs)
+
+#close file
 f.close()
