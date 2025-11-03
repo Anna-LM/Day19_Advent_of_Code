708a838aaa2248096dc441c489ff7ec9123bdce2
Anna-LM <annallambiasmaw@gmail.com>
2025-11-02 20:46:05 -0700

    attempting as recurssion

--- attemp2.py
+++ attemp2.py
@@ -20,46 +20,4 @@ desired_towels=desired_towels[1:]
 #close file
 f.close()
 
-
-
-
-## -- Create Towel designs --
-complete_towel_counter  = 0
-
-
-
-print(desired_towels)
-print(towel_inputs)
-possible_individual_stripes =  ['w','u','b','r','g']
-print()
-# remove desired towels and input stripes that can be made up of individul stripe inputs
-
-individual_stripes_not_included=[]
-
-for colour in possible_individual_stripes:
-    if colour not in towel_inputs:
-        individual_stripes_not_included.append(colour)
-print(individual_stripes_not_included)
-
-reduced_towel_inputs = []
-
-for towel_input in towel_inputs:
-    flag = False
-    for colour in individual_stripes_not_included:
-        if colour in towel_input:
-            flag = True
-    if flag == True:
-        reduced_towel_inputs.append(towel_input)
-
-index_of_incomplete=[]
-
-reduced_desired_towels = []
-for desired_towel in desired_towels:
-    flag = False
-    for colour in individual_stripes_not_included:
-        if colour in desired_towel:
-            flag = True
-    if flag == True:
-        index_of_incomplete.append()
-        reduced_desired_towels.append(desired_towel)
-
+#check if an input is in th towel, then chck if th input is in that new input and so on --> recursion
