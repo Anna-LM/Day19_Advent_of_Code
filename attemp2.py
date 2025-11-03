4ddf092d67b40a2112925bf8ce5b36e24c467671
Anna-LM <annallambiasmaw@gmail.com>
2025-11-02 19:46:21 -0700

    list of the individual stripe colours not there

--- attemp2.py
+++ attemp2.py
@@ -19,3 +19,24 @@ desired_towels=desired_towels[1:]
  
 #close file
 f.close()
+
+
+
+
+## -- Create Towel designs --
+complete_towel_counter  = 0
+
+
+
+print(desired_towels)
+print(towel_inputs)
+possible_individual_stripes =  ['w','u','b','r','g']
+print()
+# remove desired towels and input stripes that can be made up of individul stripe inputs
+
+individual_stripes_not_included=[]
+
+for colour in possible_individual_stripes:
+    if colour not in towel_inputs:
+        individual_stripes_not_included.append(colour)
+print(individual_stripes_not_included)
