8b2fcb89b1afe1d7a5592c3400f1794206e24180
Anna-LM <annallambiasmaw@gmail.com>
2025-11-03 09:22:41 -0700

    renamed variables for recurrsion

--- attemp2.py
+++ attemp2.py
@@ -22,35 +22,32 @@ desired_towels=desired_towels[1:]
 #close file
 f.close()
 
-#check if an input is in th towel, then chck if th input is in that new input and so on --> recursion
 print(desired_towels)
 print(towel_inputs)
 
-#is towel input 1 in position 1 of towel 1:
-#first_part_of_towel_1 = desired_towels[0][0:len(towel_inputs[0])]
-#print(first_part_of_towel_1)
+#check if an input is in the towel, then check if the input is in that new input and so on --> recursion
 
 @cache
 
-def is_towel_input_1_in_first_part_of_towel(desired_towel):
+def is_towel_input_n_in_next_part_of_desired_towel(desired_towel):
 
     #repeat until:
     # no more towel inputs to try
     # or
     # no more stripes in towel 
     
-    # if theres no stripes left in towel
-
+    # if theres no stripes left in towel, the towel has been completed
     if not desired_towel:
         return True
-    #try each towel input for the first part of the of the current desired towel
+    
+    #try each towel input for the first/next part of the of the current desired towel
     for towel_input in towel_inputs:
         #if the first part of the input equals the current towel input, then try the rest of the towel input
-        first_part_of_towel_1 = desired_towel[0:len(towel_input)]
-        if first_part_of_towel_1 == towel_input:  
+        first_part_of_towel_n = desired_towel[0:len(towel_input)]
+        if first_part_of_towel_n == towel_input:  
             #keep the recursion going
-            rest_of_towel = desired_towel[len(towel_input):]
-            if is_towel_input_1_in_first_part_of_towel(rest_of_towel):
+            rest_of_towel_n= desired_towel[len(towel_input):]
+            if is_towel_input_n_in_next_part_of_desired_towel(rest_of_towel_n):
                 return True
             
     #if every towel input is tried and the remaining stripes/next stripes of the towel doesnt equal an input, it cant be made
@@ -59,10 +56,9 @@ def is_towel_input_1_in_first_part_of_towel(desired_towel):
 #count how many complete towels, for each towel tried, and returned true, increase counter
 complete_towel_counter = 0
 for desired_towel in desired_towels:
-    if is_towel_input_1_in_first_part_of_towel(desired_towel):
+    if is_towel_input_n_in_next_part_of_desired_towel(desired_towel):
         complete_towel_counter = complete_towel_counter+ 1
 
 print(complete_towel_counter)
-
-            #try next towel input      
+     
 
