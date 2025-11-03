f3dd91855b27467b3ce59565218b6dfc84b7c137
Anna-LM <annallambiasmaw@gmail.com>
2025-11-02 19:44:20 -0700

    creating a new file for new method

---
+++ attemp2.py
@@ -0,0 +1,21 @@
+## -- Read input texts into useful formats --
+
+#Open file name example.txt
+f = open("example.txt")
+
+#read the first line of the file
+towel_inputs = (f.readline()).strip('\n')
+
+#format the line into a list of each towel option
+towel_inputs=towel_inputs.split(', ')
+
+desired_towels=[]
+#make a list of all the final towel designs
+for line in f:
+    desired_towels.append((line.strip('\n')))
+
+#remove first element which was blank
+desired_towels=desired_towels[1:]
+ 
+#close file
+f.close()
